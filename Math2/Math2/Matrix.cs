55ae4c5179533f9917518e544ab880c3aee3bcef
wudisluv <vlad.kornilov2015@gmail.com>
2022-09-11 23:47:36 +0300

    HW2

---
+++ Math2/Math2/Matrix.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace Math2
+{
+    public class Matrix
+    {
+        int i, j;
+        int[,] matrix;
+        public Matrix(int i, int j)
+        {
+            this.i = i;
+            this.j = j;
+            matrix = new int [i,j];
+            Random random = new Random();
+            for (int m = 0; m < this.i; m++)
+            {
+                for (int n = 0; n < this.j; n++)
+                {
+                    matrix[m, n] = random.Next(1,3);
+                }
+            }
+        }
+
+        public void MatrixSum(Matrix matrix)
+        {
+            if (this.matrix.Length <= matrix.matrix.Length)
+            {
+                for (int i = 0; i < this.i; i++)
+                {
+                    for (int j = 0; j < this.j; j++)
+                    {
+                        this.matrix[i, j] += matrix.matrix[i, j];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < matrix.i; i++)
+                {
+                    for (int n = 0; j < matrix.j; j++)
+                    {
+                        this.matrix[i, j] += matrix.matrix[i, j];
+                    }
+                }
+            }
+        }
+        public void MultiplyOnNum(int k)
+        {
+            for(int i = 0; i < this.i; i++)
+            {
+                for (int j = 0; j < this.j; j++)
+                {
+                    matrix[i, j] *= k;
+                }
+            }
+        }
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < this.i; i++)
+            {
+                for (int j = 0; j < this.j; j++)
+                {
+                    Console.Write(this.matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void MatrixMatrix(Matrix matrix)
+        {
+                int[,] tempMatrix = new int[this.i, matrix.j];
+                for (int i = 0; i < this.i; i++)
+                {
+                    for (int j = 0; j < matrix.i; j++)
+                    {
+                        for (int k = 0; k < matrix.j; k++)
+                        {
+                            tempMatrix[i, j] += this.matrix[i, k] * matrix.matrix[k, j];
+                        }
+                    }
+                }
+            this.matrix = tempMatrix;
+        }
+    }
+}
+
---
+++ Math2/Math2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Math2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix1 = new Matrix(3, 3);
+            Matrix matrix2 = new Matrix(2, 2);
+            Matrix matrix3 = new Matrix(2, 2);
+            Matrix matrix4 = new Matrix(3, 3);
+            matrix1.MatrixSum(matrix2);
+            matrix1.PrintMatrix();
+            Console.WriteLine();
+            matrix4.MultiplyOnNum(3);
+            matrix4.PrintMatrix();
+            Console.WriteLine();
+            matrix2.PrintMatrix();
+            Console.WriteLine();
+            matrix3.PrintMatrix();
+            Console.WriteLine();
+            matrix2.MatrixMatrix(matrix3);
+            matrix2.PrintMatrix();
+            Console.ReadLine();
+        }
+    }
+}
